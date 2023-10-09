b7af787a59f3a484935394b32c1a11f17a463522
Michael Aichmueller <m.aichmueller@gmail.com>
2023-10-09 19:45:19 +0200

    add Debugging Messenger class to sandbox

--- test/sandbox/sandbox.hpp
+++ test/sandbox/sandbox.hpp
@@ -1,11 +1,45 @@
 #ifndef NOR_SANDBOX_HPP
 #define NOR_SANDBOX_HPP
 
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+#include <iostream>
+#include <utility>
 
 namespace py = pybind11;
 
+struct Messenger {
+   explicit Messenger(std::string name_ = "DefaultObject") : name(std::move(name_))
+   {
+      std::cout << "CTOR - " << name << "\n";
+   }
+   ~Messenger() { std::cout << "DTOR - " << name << "\n"; }
+   Messenger(const Messenger& other) : name(other.name) { std::cout << "COPY - " << name << "\n"; }
+   Messenger(Messenger&& other) noexcept : name(std::move(other.name))
+   {
+      std::cout << "MOVE - " << name << "\n";
+   }
+   Messenger& operator=(const Messenger& other)
+   {
+      if(this == &other) {
+         return *this;
+      }
+      name = other.name;
+      std::cout << "COPY assignment - " << name << "\n";
+      return *this;
+   }
+   Messenger& operator=(Messenger&& other) noexcept
+   {
+      if(this == &other) {
+         return *this;
+      }
+      name = std::move(other.name);
+      std::cout << "MOVE assignment - " << name << "\n";
+      return *this;
+   }
 
+   std::string name;
+};
 
 #endif  // NOR_SANDBOX_HPP
