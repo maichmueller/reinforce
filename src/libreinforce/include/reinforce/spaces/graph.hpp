14fffe3a43df25681e9027042ddfcef9ed1a57c8
Michael Aichmueller <m.aichmueller@gmail.com>
2024-04-15 19:30:03 +0200

    working graph sample function for non masked options / edges (untested)

--- src/libreinforce/include/reinforce/spaces/graph.hpp
+++ src/libreinforce/include/reinforce/spaces/graph.hpp
@@ -6,6 +6,8 @@
 #include <optional>
 #include <reinforce/utils/views_extension.hpp>
 
+#include "reinforce/utils/macro.hpp"
+#include "reinforce/utils/xarray_formatter.hpp"
 #include "reinforce/utils/xtensor_typedefs.hpp"
 
 namespace force {
@@ -23,14 +25,32 @@ struct GraphInstance {
    /// An (m x 2) sized array of ints.
    idx_xarray edge_links;
 };
+}  // namespace force
 
+template < typename... Args >
+struct fmt::formatter< ::force::GraphInstance< Args... > > {
+   template < typename FormatContext >
+   auto format(const ::force::GraphInstance< Args... >& graph, FormatContext& ctx) const
+   {
+      return fmt::format_to(
+         ctx.out(),
+         "GraphInstance(nodes={}, edges={}, edge_links={})",
+         graph.nodes,
+         graph.edges,
+         graph.edge_links
+      );
+   }
+   static constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+};
+
+namespace force {
 template < typename NodeSpace, typename EdgeSpace >
 concept graph_space_concept =
    (detail::is_specialization_v< NodeSpace, BoxSpace >
     or detail::is_specialization_v< NodeSpace, DiscreteSpace >)
    and (detail::is_specialization_v< EdgeSpace, BoxSpace > or detail::is_specialization_v< EdgeSpace, DiscreteSpace >);
 
-template < typename NodeSpace, typename EdgeSpace = DiscreteSpace< bool > >
+template < typename NodeSpace, typename EdgeSpace = DiscreteSpace< short > >
    requires graph_space_concept< NodeSpace, EdgeSpace >
 class GraphSpace:
     public Space<
@@ -65,68 +85,67 @@ class GraphSpace:
    {
    }
 
+   const auto& node_space() const { return m_node_space; }
+   const auto& edge_space() const { return m_edge_space; }
+
   private:
    NodeSpace m_node_space;
    std::optional< EdgeSpace > m_edge_space;
 
-   value_type _sample(
-      std::nullopt_t = std::nullopt,
-      size_t num_nodes = 10,
-      std::optional< size_t > num_edges = std::nullopt
-   ) const
-   {
-      return _sample(std::tuple{std::nullopt, std::nullopt}, num_nodes, num_edges);
-   }
+   //   value_type _sample(
+   //      std::nullopt_t = std::nullopt,
+   //      size_t num_nodes = 10,
+   //      std::optional< size_t > num_edges = std::nullopt
+   //   ) const
+   //   {
+   //      return _sample(std::tuple{std::nullopt, std::nullopt}, num_nodes, num_edges);
+   //   }
 
-   template < typename node_mask_t = std::nullopt_t, typename edge_mask_t = std::nullopt_t >
-   value_type _sample(
-      const std::tuple< node_mask_t, edge_mask_t >& mask,
-      size_t num_nodes = 10,
-      std::optional< size_t > num_edges = std::nullopt
-   ) const;
+   //   template < typename node_mask_t = std::nullopt_t, typename edge_mask_t = std::nullopt_t >
+   //   value_type _sample(
+   //      const std::tuple< node_mask_t, edge_mask_t >& mask = std::tuple{std::nullopt,
+   //      std::nullopt}, size_t num_nodes = 10, std::optional< size_t > num_edges = std::nullopt
+   //   ) const;
 
    template <
       typename node_mask_t = std::nullopt_t,
       typename edge_mask_t = std::nullopt_t,
       typename size_or_range_t = size_t,
-      typename optional_size_or_range_t = std::nullopt_t >
-      requires detail::is_specialization_v< optional_size_or_range_t, std::optional >
+      typename optional_size_or_range_t = std::optional< short > >
+      requires(detail::
+                  is_specialization_v< detail::raw_t< optional_size_or_range_t >, std::optional >)
    multi_value_type _sample(
       size_t nr_samples,
-      const std::tuple< node_mask_t, edge_mask_t >& mask = std::tuple{std::nullopt, std::nullopt},
+      const std::tuple< node_mask_t, edge_mask_t >& mask,
       size_or_range_t&& num_nodes = 10,
       optional_size_or_range_t&& num_edges = std::nullopt
    ) const;
 
-   //   auto _generate_node_sample_space(size_t num_nodes) const;
-   //   auto _generate_edge_sample_space(size_t num_edges) const;
+   template < typename... Args >
+   auto _sample(size_t nr_samples, std::nullopt_t mask = std::nullopt, Args&&... args) const
+   {
+      return _sample(nr_samples, std::tuple{mask, mask}, FWD(args)...);
+   }
+
+   idx_xarray _sample_edge_links(size_t num_nodes, size_t num_edges) const
+   {
+      if(num_edges == 0) {
+         return idx_xarray::from_shape({0});
+      }
+      return xt::random::randint< size_t >({num_edges, 2ul}, size_t{0}, num_nodes, rng());
+   }
 };
 
 // template < typename NodeSpace, typename EdgeSpace >
 //    requires graph_space_concept< NodeSpace, EdgeSpace >
-// auto GraphSpace< NodeSpace, EdgeSpace >::_generate_node_sample_space(size_t num_nodes) const
+// template < typename node_mask_t, typename edge_mask_t >
+// GraphSpace< NodeSpace, EdgeSpace >::value_type GraphSpace< NodeSpace, EdgeSpace >::_sample(
+//    const std::tuple< node_mask_t, edge_mask_t >& mask,
+//    size_t num_nodes,
+//    std::optional< size_t > num_edges
+//) const
 //{
-//    if constexpr(detail::is_specialization_v< NodeSpace, DiscreteSpace >) {
-//       return MultiDiscreteSpace< detail::data_t< NodeSpace > >{
-//          ranges::views::repeat_n(m_node_space.n(), num_nodes), rng()
-//       };
-//    } else {
-//       return BoxSpace< detail::data_t< NodeSpace > >{
-//          ranges::views::repeat_n(m_node_space.n(), num_nodes), rng()
-//       };
-//    }
 // }
-
-template < typename NodeSpace, typename EdgeSpace >
-   requires graph_space_concept< NodeSpace, EdgeSpace >
-template < typename node_mask_t, typename edge_mask_t >
-GraphSpace< NodeSpace, EdgeSpace >::value_type GraphSpace< NodeSpace, EdgeSpace >::_sample(
-   const std::tuple< node_mask_t, edge_mask_t >& mask,
-   size_t num_nodes,
-   std::optional< size_t > num_edges
-) const
-{
-}
 
 template < typename NodeSpace, typename EdgeSpace >
    requires graph_space_concept< NodeSpace, EdgeSpace >
@@ -135,7 +154,7 @@ template <
    typename edge_mask_t,
    typename size_or_forwardrange_t,
    typename optional_size_or_range_t >
-   requires detail::is_specialization_v< optional_size_or_range_t, std::optional >
+   requires(detail::is_specialization_v< detail::raw_t< optional_size_or_range_t >, std::optional >)
 auto GraphSpace< NodeSpace, EdgeSpace >::_sample(
    size_t nr_samples,
    const std::tuple< node_mask_t, edge_mask_t >& mask,
@@ -143,12 +162,18 @@ auto GraphSpace< NodeSpace, EdgeSpace >::_sample(
    optional_size_or_range_t&& num_edges
 ) const -> multi_value_type
 {
-   auto&& [node_space_mask, edge_space_mask] = mask;
+   const bool has_edge_space = m_edge_space.has_value();
+   const auto& [node_space_mask, edge_space_mask] = mask;
    // build the numbers of edges array out of the possible parameter combinations of
    // `num_nodes` and `num_edges`
    auto [num_nodes_view, num_nodes_view_size] = std::invoke([&] {
       if constexpr(std::is_unsigned_v< size_or_forwardrange_t >) {
-         return std::pair{ranges::views::repeat_n(num_nodes, nr_samples), nr_samples};
+         return std::pair{
+            ranges::views::repeat_n(
+               static_cast< size_t >(num_nodes), static_cast< long >(nr_samples)
+            ),
+            nr_samples
+         };
       } else {
          auto size = std::ranges::size(num_nodes);
          if(size != nr_samples) {
@@ -158,32 +183,49 @@ auto GraphSpace< NodeSpace, EdgeSpace >::_sample(
                nr_samples
             ));
          }
-         return std::pair{ranges::views::all(num_nodes), size};
+         return std::pair{num_nodes | ranges::views::cast< size_t >, size};
       }
    });
    xarray< size_t > num_edges_arr = std::invoke([&] {
       xarray< size_t > out = xt::empty< size_t >({nr_samples});
       if(num_edges.has_value()) {
-         if(not m_edge_space.has_value()) {
+         if(not has_edge_space) {
             SPDLOG_WARN(
                fmt::format("The number of edges is set, but the edge space is None.", num_edges)
             );
             out = 0;
          } else {
-            if constexpr(std::unsigned_integral< detail::raw_t< optional_size_or_range_t > >) {
-               if(*num_edges == 0) {
-                  throw std::invalid_argument("`num_edges` parameter needs to be greater than 0.");
-               }
-            } else {
+            if constexpr(std::ranges::range<
+                            detail::value_t< detail::raw_t< optional_size_or_range_t > > >) {
                if(ranges::any_of(*num_edges, [](const auto& val) { return val == 0; })) {
                   throw std::invalid_argument(
                      "`num_edges` parameter needs to be greater than 0 for every sample."
                   );
                }
             }
+            static_assert(
+               std::integral< detail::value_t< detail::raw_t< optional_size_or_range_t > > >,
+               "If not a range, the num_edges parameter needs to be of integral size type."
+            );
+            if(std::cmp_greater(*num_edges, 0)) {
+               throw std::invalid_argument("`num_edges` parameter needs to be greater than 0.");
+            }
          }
       } else {
-         if constexpr(std::unsigned_integral< detail::raw_t< size_or_forwardrange_t > >) {
+         if constexpr(std::ranges::forward_range< detail::raw_t< size_or_forwardrange_t > >) {
+            for(auto [i, n_nodes] :
+                ranges::views::enumerate(num_nodes | ranges::views::cast< size_t >)) {
+               // as per gymnasium doc:
+               // max number of edges is `n*(n-1)` with self connections and two-way is allowed
+               auto& entry = out.unchecked(i);
+               entry = xt::random::randint({1}, size_t{0}, n_nodes * (n_nodes - 1), rng())
+                          .unchecked(0);
+            }
+         } else {
+            static_assert(
+               std::integral< detail::raw_t< size_or_forwardrange_t > >,
+               "If not a forward range, the num_nodes parameter needs to be an integral size type."
+            );
             if(std::cmp_greater(num_nodes, 1)) {
                out = xt::random::randint(
                   {nr_samples}, size_t{0}, static_cast< size_t >(num_nodes), rng()
@@ -191,59 +233,61 @@ auto GraphSpace< NodeSpace, EdgeSpace >::_sample(
             } else {
                out = size_t{0};
             }
-         } else {
-            for(auto [i, n_nodes] :
-                ranges::views::enumerate(num_nodes | ranges::views::cast< size_t >)) {
-               // as per gymnasium doc:
-               // max number of edges is `n*(n-1)` with self connections and two-way is allowed
-               auto& entry = out.unchecked(i);
-               entry = xt::random::randint({1}, size_t{0}, n_nodes * (n_nodes - 1), rng());
-            }
          }
       }
       return out;
    });
-   size_t total_nr_node_samples = ranges::accumulate(num_nodes_view);
+   FORCE_DEBUG_ASSERT(std::ranges::size(num_nodes_view) == num_edges_arr.size());
+   size_t total_nr_node_samples = ranges::accumulate(num_nodes_view, size_t{0}, std::plus{});
    size_t total_nr_edge_samples = xt::sum(num_edges_arr).unchecked(0);
    auto sampled_nodes = m_node_space.sample(total_nr_node_samples, node_space_mask);
-   auto sampled_edges = m_edge_space.has_value()
+   auto sampled_edges = has_edge_space
                            ? m_edge_space->sample(total_nr_edge_samples, edge_space_mask)
-                           : detail::multi_value_t< EdgeSpace >{};
+                           : detail::multi_value_t< EdgeSpace >::from_shape({0});
 
    if(nr_samples == 1) {
       return std::vector{value_type{
          .nodes = std::move(sampled_nodes),
          .edges = std::move(sampled_edges),
-         .edge_links = _sample_edge_links(num_nodes, num_edges)
+         .edge_links = _sample_edge_links(
+            *std::ranges::begin(num_nodes_view), has_edge_space and num_edges_arr.unchecked(0)
+         )
       }};
    } else {
       multi_value_type samples;
       samples.reserve(nr_samples);
-      auto shared_node_buffer = new detail::data_t< NodeSpace >[total_nr_node_samples];
-      auto shared_edge_buffer = m_edge_space.has_value()
-                                   ? new detail::data_t< EdgeSpace >[total_nr_edge_samples]
-                                   : nullptr;
-      auto [node_offset, edge_offset] = std::array{0, 0};
+      auto node_space_nr_elements_per_sample = ranges::accumulate(
+         m_node_space.shape(), size_t{1}, std::multiplies{}
+      );
+      auto edge_space_nr_elements_per_sample = has_edge_space ? ranges::accumulate(
+                                                                   m_node_space.shape(),
+                                                                   size_t{1},
+                                                                   std::multiplies{}
+                                                                )
+                                                              : 0;
+
+      auto [node_offset, edge_offset] = std::array{0ul, 0ul};
       for(auto [n_nodes, n_edges] : ranges::views::zip(num_nodes_view, num_edges_arr)) {
-         auto start_nodes = std::exchange(node_offset, node_offset + n_nodes);
-         auto start_edges = std::exchange(edge_offset, edge_offset + n_edges);
+         size_t node_slice_size = n_nodes * node_space_nr_elements_per_sample;
+         size_t edge_slice_size = n_edges * edge_space_nr_elements_per_sample;
+         auto start_nodes = std::exchange(node_offset, node_offset + node_slice_size);
+         auto start_edges = std::exchange(edge_offset, edge_offset + edge_slice_size);
+
          samples.emplace_back(value_type{
-            .nodes = xt::adapt(
-               std::advance(shared_node_buffer.get(), start_nodes),
-               n_nodes,
-               xt::acquire_ownership(),
-               xt::svector< size_t >{n_nodes} + m_node_space->shape()
+            .nodes = xt::strided_view(
+               sampled_nodes, {xt::ellipsis(), xt::range(start_nodes, node_offset)}
             ),
-            .edges = m_edge_space.has_value()
-                        ? xt::adapt(
-                             std::advance(shared_edge_buffer.get(), start_edges),
-                             n_edges,
-                             xt::acquire_ownership(),
-                             xt::svector< size_t >{n_edges} + m_edge_space->shape()
-                          )
-                        : detail::multi_value_t< EdgeSpace >{},
-            .edge_links = m_edge_space.has_value() ? _sample_edge_links(n_nodes, n_edges)
-                                                   : idx_xarray{},
+            .edges = std::invoke([&]() -> detail::multi_value_t< EdgeSpace > {
+               if(has_edge_space) {
+                  return xt::strided_view(
+                     sampled_edges, {xt::ellipsis(), xt::range(start_edges, edge_offset)}
+                  );
+               } else {
+                  return detail::multi_value_t< EdgeSpace >::from_shape({0});
+               }
+            }),
+            .edge_links = has_edge_space ? _sample_edge_links(n_nodes, n_edges)
+                                         : idx_xarray::from_shape({0}),
          });
       }
       return samples;
--- test/libreinforce/test_space_graph.cpp
+++ test/libreinforce/test_space_graph.cpp
@@ -1,9 +1,10 @@
 #include <cstddef>
-#include <reinforce/spaces/box.hpp>
-#include <reinforce/spaces/discrete.hpp>
+#include <optional>
 
 #include "gtest/gtest.h"
 #include "pybind11/embed.h"
+#include "reinforce/spaces/box.hpp"
+#include "reinforce/spaces/discrete.hpp"
 #include "reinforce/spaces/graph.hpp"
 #include "reinforce/utils/math.hpp"
 #include "reinforce/utils/xarray_formatter.hpp"
@@ -16,4 +17,34 @@ TEST(Spaces, Graph_Discrete_Discrete_construction)
    EXPECT_NO_THROW((GraphSpace{DiscreteSpace{10, 0}}));
    EXPECT_NO_THROW((GraphSpace{DiscreteSpace{10, 0}, DiscreteSpace{5, 0}}));
    EXPECT_NO_THROW((GraphSpace{DiscreteSpace{10, 0}, DiscreteSpace{5, 0}, 45466}));
+}
+
+TEST(Spaces, Graph_Discrete_None_sample)
+{
+   auto space = GraphSpace{DiscreteSpace{5, 0}, 553};
+   constexpr auto n_samples = 20;
+   auto verify_samples = [&](const auto& samples, auto num_nodes) {
+      SPDLOG_DEBUG(fmt::format("Samples:\n{}", fmt::join(samples, "\n")));
+      for(const auto& [sample, n_nodes_expected] : ranges::views::zip(samples, num_nodes)) {
+         EXPECT_EQ(sample.nodes.size(), n_nodes_expected);
+         EXPECT_TRUE(xt::all(xt::greater_equal(sample.nodes, space.node_space().start())));
+         EXPECT_TRUE(xt::all(
+            xt::less_equal(sample.nodes, space.node_space().start() + space.node_space().n())
+         ));
+         EXPECT_EQ(sample.edges.size(), 0);
+         EXPECT_EQ(sample.edge_links.size(), 0);
+      }
+   };
+   auto samples = space.sample(n_samples);
+   verify_samples(samples, ranges::views::repeat_n(10ul, long(n_samples)));
+   samples = space.sample(
+      n_samples, std::nullopt, ranges::views::indices(2, 22)
+      //            xt::random::randint({n_samples}, 2, 10, space.rng())
+   );
+   verify_samples(samples, ranges::views::indices(2, 22));
+
+   for([[maybe_unused]] auto _ : ranges::views::iota(0, 100)) {
+      auto sample = space.sample();
+      verify_samples(ranges::views::repeat_n(sample, 1), ranges::views::repeat_n(10, 1));
+   }
 }
