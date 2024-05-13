0f1177bb256d0e0598148cad67c3d3f6e845d8af
Michael Aichmueller <m.aichmueller@gmail.com>
2024-05-13 08:32:19 +0200

    update concate function

--- src/libreinforce/include/reinforce/utils/concatenate.hpp
+++ src/libreinforce/include/reinforce/utils/concatenate.hpp
@@ -12,21 +12,8 @@
 
 namespace force {
 
-template < typename Space >
-struct concatenate {
-   template < typename ValueRange >
-   auto operator()(const Space& space, ValueRange&& items) const
-   {
-      return ranges::to< std::vector< detail::batch_value_t< Space > > >(FWD(items));
-   }
-
-   template < typename ValueRange, typename OutT >
-      requires detail::is_specialization_v< detail::raw_t< OutT >, std::vector >
-   auto& operator()(const Space& space, ValueRange&& items, OutT& out) const
-   {
-      return ranges::move(out, std::ranges::begin(out), (FWD(items)));
-   }
-};
+template < typename T >
+struct concatenate;
 
 /// generic specialization for undefined space types (similar to std::plus, std::minus, etc.)
 /// will redirect to the respective implementation based on the provided space type.
@@ -45,20 +32,47 @@ struct concatenate< void > {
    }
 };
 
-template < typename SpaceT >
-   requires(detail::is_space< SpaceT >
+/// default implementation for concatenating values in a space to a vector over value_type
+template < typename Space >
+   requires detail::is_space< Space >
+struct concatenate< Space > {
+   static_assert(detail::is_space< Space >, "Space must be a space type");
+
+   using value_type = detail::value_t< Space >;
+   using batch_value_type = detail::batch_value_t< Space >;
+   using data_type = detail::data_t< Space >;
+   using result_type = std::vector< value_type >;
+
+   template < typename ValueRange >
+   result_type operator()(const Space& space, ValueRange&& items) const
+   {
+      return ranges::to< result_type >(FWD(items));
+   }
+
+   template < typename ValueRange, typename OutT >
+      requires detail::is_specialization_v< detail::raw_t< OutT >, std::vector >
+   auto& operator()(const Space& space, ValueRange&& items, OutT& out) const
+   {
+      return ranges::move(out, std::ranges::begin(out), (FWD(items)));
+   }
+};
+
+/// specialization for spaces over xarrays
+/// concatenates range of xarrays to a single xarray of shape (n, ...)
+template < typename Space >
+   requires(detail::is_space< Space >
             and detail::is_any_v< //
-               SpaceT, //
-               DiscreteSpace< detail::data_t< SpaceT > >,
-               BoxSpace< detail::data_t< SpaceT > >,
-               MultiDiscreteSpace< detail::data_t< SpaceT > >,
+               Space, //
+               DiscreteSpace< detail::data_t< Space > >,
+               BoxSpace< detail::data_t< Space > >,
+               MultiDiscreteSpace< detail::data_t< Space > >,
                MultiBinarySpace >
    )
-struct concatenate< SpaceT > {
-   using space_type = SpaceT;
-   using value_type = detail::value_t< SpaceT >;
-   using batch_value_type = detail::batch_value_t< SpaceT >;
-   using data_type = detail::data_t< SpaceT >;
+struct concatenate< Space > {
+   using space_type = Space;
+   using value_type = detail::value_t< Space >;
+   using batch_value_type = detail::batch_value_t< Space >;
+   using data_type = detail::data_t< Space >;
 
    template < std::ranges::sized_range ValueRange >
       requires std::ranges::forward_range< detail::raw_t< ValueRange > >
@@ -91,19 +105,23 @@ struct concatenate< SpaceT > {
    }
 };
 
-template < typename SpaceT >
-   requires(detail::is_space< SpaceT >
-            and SpaceT::is_composite_space
-            and requires(SpaceT space) { typename SpaceT::spaces_tuple_type; }
+/// specialization for TupleSpace
+/// concatenates range of tuples to a single tuple with individually
+/// concatenated value types as concatenation goes for each subspace
+template < typename Space >
+   requires(detail::is_space< Space >
+            and Space::is_composite_space
+            and requires(Space space) { typename Space::spaces_tuple_type; }
             and std::same_as< //
-               SpaceT, //
-               TupleSpace< typename SpaceT::spaces_tuple_type > >
+               Space, //
+               TupleSpace< typename Space::spaces_tuple_type > >
    )
-struct concatenate< SpaceT > {
-   using space_type = SpaceT;
-   using value_type = detail::value_t< SpaceT >;
-   using batch_value_type = detail::batch_value_t< SpaceT >;
-   using data_type = detail::data_t< SpaceT >;
+struct concatenate< Space > {
+   using space_type = Space;
+   using value_type = detail::value_t< Space >;
+   using batch_value_type = detail::batch_value_t< Space >;
+   using data_type = detail::data_t< Space >;
+   using result_type = batch_value_type;
 
    static constexpr auto spaces_idx_seq = std::make_index_sequence<
       std::tuple_size_v< typename space_type::spaces_tuple_type > >{};
@@ -140,49 +158,6 @@ struct concatenate< SpaceT > {
          },
          spaces_idx_seq
       );
-   }
-};
-
-template < typename SpaceT >
-   requires(detail::is_space< SpaceT >
-            and detail::is_any_v< //
-               SpaceT, //
-               TupleSpace< detail::data_t< SpaceT > > >
-   )
-struct concatenate< SpaceT > {
-   using space_type = SpaceT;
-   using value_type = detail::value_t< SpaceT >;
-   using batch_value_type = detail::batch_value_t< SpaceT >;
-   using data_type = detail::data_t< SpaceT >;
-
-   template < std::ranges::sized_range ValueRange >
-      requires std::ranges::forward_range< detail::raw_t< ValueRange > >
-               and std::convertible_to<
-                  std::ranges::range_reference_t< detail::raw_t< ValueRange > >,
-                  value_type >
-   batch_value_type operator()(const space_type& space, ValueRange&& items)
-   {
-      value_type out = xt::empty< data_type >(prepend({items.size()}, space.shape()));
-      stack(space, FWD(items), out);
-      return out;
-   }
-   template < std::ranges::sized_range ValueRange >
-      requires std::convertible_to< std::ranges::range_reference_t< ValueRange >, value_type >
-   batch_value_type& operator()(const space_type& space, ValueRange&& items, batch_value_type& out)
-   {
-      stack(space, FWD(items), out);
-      return out;
-   }
-
-  private:
-   template < std::ranges::sized_range ValueRange >
-   batch_value_type& stack(const space_type& space, ValueRange&& items, batch_value_type& out)
-   {
-      size_t idx = 0;
-      for(auto&& item : FWD(items)) {
-         xt::view(out, idx, xt::ellipsis()) = FWD(item);
-         ++idx;
-      }
    }
 };
 
